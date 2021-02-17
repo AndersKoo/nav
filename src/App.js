7f4348033b706f3a84c7bea3c9255e77395e48cf
Anders Koo Furulund <anderskoo@live.no>
2021-02-17 14:35:34 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import React from "react";
 import Jobs from "./components/Jobs/Jobs";
 import Navbar from "./components/Navbar/Navbar";
 
-// https://www.youtube.com/watch?v=ZlDASfsL7FI
-// https://www.youtube.com/watch?v=ALqckoyivq4
-// https://www.youtube.com/watch?v=HANSMtDy508
-// https://www.npmjs.com/package/react-paginate
-
 function App() {
   return (
     <div className="app">
